ec39c1e00e69c28e533f545dad0dfd81547db2fa
Lith1um <alexrayner94@gmail.com>
2024-08-11 15:37:46 +0100

    remove zone.js

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -8,7 +8,7 @@ import { TypedPocketBase } from '../../pocketbase-types';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(),
     {
